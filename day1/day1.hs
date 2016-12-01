383dd9d663cc76bd04cf5eced303bf68bb1a2d0e
Paulius Imbrasas <cremboc@gmail.com>
2016-12-01 13:58:31 +0000

    Day 1 Part 2 done

--- day1/day1.hs
+++ day1/day1.hs
@@ -3,77 +3,69 @@
 import Data.List.Split
 import Text.Regex.Posix
 
-data Facing = N | S | E | W deriving (Read, Show, Enum, Eq, Ord)
-data Direction = R | L deriving (Read, Show, Enum, Eq, Ord)
+data Facing = N | S | E | W deriving Eq
+data Direction = R | L deriving Eq
 type Steps = Int
 type Move = (Direction, Steps)
 type Coordinate = (Int, Int)
 
-move :: Move -> Facing -> Coordinate -> (Facing, Coordinate)
-move mv facing xy
-    | facing == N = case nxtDir of
-        R -> (E, (x + steps, y))
-        L -> (W, (x - steps, y))
-    | facing == S = case nxtDir of
-        R -> (W, (x - steps, y))
-        L -> (E, (x + steps, y))
-    | facing == E = case nxtDir of
-        R -> (S, (x, y - steps))
-        L -> (N, (x, y + steps))
-    | facing == W = case nxtDir of
-        R -> (N, (x, y + steps))
-        L -> (S, (x, y - steps))
+parseMove :: String -> Move
+parseMove mv = (direction, read steps :: Int)
     where
-        (x, y) = xy
-        (nxtDir, steps) = mv
-
-createMove :: String -> Move
-createMove mv = (direction, read steps :: Int)
-    where
-        direction = case dir of
-            "R" -> R
-            "L" -> L
+        direction = if dir == "R" then R else L
         (_, _, _, [dir, steps]) = mv =~ "(R|L)([0-9]+)" :: (String, String, String, [String])
 
-doMoves :: [Move] -> Facing -> Coordinate -> Coordinate
-doMoves moves facing loc
-    | length moves == 0 = loc
-    | otherwise = doMoves ms newFacing newLoc
+nextLocation :: Coordinate -> Facing -> Coordinate
+nextLocation location direction =  case direction of
+    N -> (x, succ y)
+    S -> (x, pred y)
+    E -> (succ x, y)
+    W -> (pred x, y)
     where
-        (newFacing, newLoc) = move m facing loc
-        (m : ms) = moves
+        (x, y) = location
 
-followPath :: [Move] -> [Coordinate] -> Facing -> Coordinate -> Coordinate
-followPath moves locs facing loc
-    | length moves == 0 = loc
-    | newLoc `elem` locs = newLoc
-    | otherwise = followPath ms newLocs newFacing newLoc
+step :: Int -> [Coordinate] -> Facing -> [Coordinate]
+step steps path direction =
+    if steps == 0
+        then tail path
+        else step (pred steps) (path ++ [loc]) direction
     where
-        (newFacing, newLoc) = move m facing loc
+        loc = nextLocation (last path) direction
+
+doMove :: Move -> Facing -> Coordinate -> (Facing, [Coordinate])
+doMove move facing location = (newDirection, step steps [location] newDirection)
+    where
+        (nxtDir, steps) = move
+        newDirection = case facing of
+            N -> if nxtDir == R then E else W
+            S -> if nxtDir == R then W else E
+            E -> if nxtDir == R then S else N
+            W -> if nxtDir == R then N else S
+
+createPath :: [Move] -> Facing -> [Coordinate] -> [Coordinate]
+createPath moves facing path
+    | length moves == 0 = path
+    | otherwise = createPath ms newFacing (path ++ newLoc)
+    where
+        (newFacing, newLoc) = doMove m facing (last path)
         (m : ms) = moves
-        newLocs = locs ++ [newLoc]
-
-    -- do move
-    -- record new coord
-    -- coord seen before? done
-
-
-manhattanDist p1 p2 =
-  sum $ zipWith (\x y -> abs (x - y)) p1 p2
 
 day1p1 = do
     input <- readFile "day1.txt"
-    return $ let moves = map createMove (splitOn ", " input) in
-        doMoves moves N (0, 0)
+    return $ let moves = map parseMove (splitOn ", " input) in
+        last $ createPath moves N [(0, 0)]
+
+findFirstDuplicate :: [Coordinate] -> Coordinate
+findFirstDuplicate coords = let c : cs = coords in
+    if c `elem` cs then c else findFirstDuplicate cs
 
 day1p2 = do
     input <- readFile "day1.txt"
-    return $ let moves = map createMove (splitOn ", " input) in
-        followPath moves [] N (0, 0)
-
+    return $ let moves = map parseMove (splitOn ", " input) in
+        findFirstDuplicate $ createPath moves N [(0, 0)]
 
 main = do
     val <- day1p1
-    let (x, y) = val in print $ manhattanDist [0, 0] [x, y]
+    let (x, y) = val in print $ abs x + abs y
     val2 <- day1p2
-    let (x, y) = val2 in print $ manhattanDist [0, 0] [x, y]
+    let (x, y) = val2 in print $ abs x + abs y
