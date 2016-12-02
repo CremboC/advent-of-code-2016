e816dde00c31a65c3cb9b2c44cb8765ab1656957
crembo <cremboc@gmail.com>
2016-12-02 09:45:00 +0000

    Day 2 cleanup

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-
-import Data.List.Split
 import qualified Data.Map.Strict as Map
 
 data Direction = U | D | L | R deriving Show
@@ -18,15 +14,9 @@ dir' 'R' = R
 
 -- map for Part 1
 locs' = Map.fromList [
-        ((-1, 1), 1),
-        ((0, 1), 2),
-        ((1, 1), 3),
-        ((-1, 0), 4),
-        ((0, 0), 5),
-        ((1, 0), 6),
-        ((-1, -1), 7),
-        ((0, -1), 8),
-        ((1, -1), 9)
+        ((-1, 1), 1), ((0, 1), 2), ((1, 1), 3),
+        ((-1, 0), 4), ((0, 0), 5), ((1, 0), 6),
+        ((-1, -1), 7), ((0, -1), 8), ((1, -1), 9)
     ]
 
 -- (a, b) -> (b, a)
@@ -35,24 +25,14 @@ locs = Map.fromList $ map (\x -> (snd x, fst x)) (Map.assocs locs')
 -- map for Part 2
 locs2' = Map.fromList [
         ((0, 2), 1),
-        ((-1, 1), 2),
-        ((0, 1), 3),
-        ((1, 1), 4),
-        ((-2, 0), 5),
-        ((-1, 0), 6),
-        ((0, 0), 7),
-        ((1, 0), 8),
-        ((2, 0), 9),
-        ((-1, -1), 0xA),
-        ((0, -1), 0xB),
-        ((1, -1), 0xC),
+        ((-1, 1), 2), ((0, 1), 3), ((1, 1), 4),
+        ((-2, 0), 5), ((-1, 0), 6), ((0, 0), 7), ((1, 0), 8), ((2, 0), 9),
+        ((-1, -1), 0xA), ((0, -1), 0xB), ((1, -1), 0xC),
         ((0, -2), 0xD)
     ]
 
 -- (a, b) -> (b, a)
 locs2 = Map.fromList $ map (\x -> (snd x, fst x)) (Map.assocs locs2')
-
-
 
 move :: Int -> Direction -> Loc -> Loc' -> Int
 move start dir lc lc' = if end == 0 then start else end
@@ -68,6 +48,7 @@ move start dir lc lc' = if end == 0 then start else end
 step :: Instruction -> Int -> Loc -> Loc' -> Int
 step instr start lc lc' = foldl (\pos ins -> move pos ins lc lc') start instr
 
+compute :: Loc -> Loc' -> IO [Int]
 compute loc loc' = do
     input <- readFile "day2.txt"
     let ins = lines input
@@ -86,6 +67,7 @@ locp2 n = Map.findWithDefault (-999, -999) n locs2
 locp2' :: Loc'
 locp2' n = Map.findWithDefault 0 n locs2'
 
+main :: IO ()
 main = do
     val <- compute locp1 locp1'
     print $ val
