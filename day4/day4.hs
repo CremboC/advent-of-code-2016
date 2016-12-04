3b60d8ee2207fcf1e8830380c483bf0d5c8dcb61
Paulius Imbrasas <cremboc@gmail.com>
2016-12-04 11:24:05 +0000

    Day 4 Part 2 done

--- day4/day4.hs
+++ day4/day4.hs
@@ -2,41 +2,48 @@ import Data.Char
 import Data.List
 import Data.Ord
 import Data.Monoid
+import Debug.Trace
 
 type Room = (String, Int, String)
 
 roomify :: String -> Room
 roomify r = (room, sector, checksum)
     where
-        room = filter (isAlpha) rm
-        sector = read $ filter (isDigit) xs
-        checksum = filter isAlpha $ dropWhile (/='[') xs
-        (rm, xs) = break (isDigit) r
+        room = init rm -- the init removes the last dash before the digits
+        sector = (read . filter isDigit) xs
+        checksum = (filter isAlpha . dropWhile (/='[')) xs
+        (rm, xs) = break isDigit r
 
--- The function mconcat simply scans through the list and obtains the first
+-- The function mconcat scans through the list and obtains the first
 -- non-EQ occurence (or EQ if all elements are EQ and thus both pairs are considered equal).
 srt (a1, a2) (b1, b2) = mconcat [compare b1 a1, compare a2 b2]
 
-checksum' :: String -> String
-checksum' room = chsm
+mkChecksum :: String -> String
+mkChecksum room = chsm
     where
         chsm = map (snd) cms
-        cms = (take 5 . sortBy (srt) . map (\g -> (length g, head g)) . group . sort) $ room
+        cms = (take 5 . sortBy (srt) . map (\g -> (length g, head g)) . group . sort . filter isAlpha) $ room
 
 isRoom :: Room -> Bool
-isRoom (room, _, checksum) = checksum == checksum' room
+isRoom (room, _, checksum) = checksum == mkChecksum room
+
+rotate :: Int -> Char -> Char
+rotate amount '-' = ' '
+rotate amount char
+    | amount == 0 = char
+    | otherwise = if succ char == succ 'z'
+        then rotate (amount - 1) 'a'
+        else rotate (amount - 1) (succ char)
+
+maxJump = ord 'z' - ord 'a'
+
+decipher :: Room -> String
+decipher (room, sector, _) = map (rotate $ sector `quot` maxJump) room
 
 main = do
     input <- readFile "input.txt"
-    let rooms = map roomify (lines input)
-    print $ foldl (\acc (_, sector, _) -> acc + sector) 0 $ filter isRoom rooms
-
-
-
-
-
-
-
-
-
-
+    let rooms = filter isRoom $ map roomify $ lines input
+    -- part 1
+    print $ foldl (\acc (_, sector, _) -> acc + sector) 0 rooms
+    -- part 2
+    print $ filter (("north" `isInfixOf`) . decipher) rooms
