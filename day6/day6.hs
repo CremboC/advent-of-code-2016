693768e342de5da9c89e6455ab38df81537644bd
Paulius Imbrasas <cremboc@gmail.com>
2016-12-06 08:59:25 +0000

    Day 6 Part 2 done

--- day6/day6.hs
+++ day6/day6.hs
@@ -1,12 +1,13 @@
 import Data.List
-import Data.Ord
-import Data.Map (Map)
-import qualified Data.Map.Strict as Map
+import Data.Ord (comparing)
 
 grp :: String -> [(Char, Int)]
 grp str = map (\l -> (head l, length l)) . group . sort $ str
 
 main :: IO ()
 main = do
-    input <- transpose . lines <$> readFile "input.txt"
-    print $ map fst $ map (maximumBy (comparing snd)) $ map grp input
+    input <- map grp . transpose . lines <$> readFile "input.txt"
+    -- part 1
+    print $ map fst . map (maximumBy (comparing snd)) $ input
+    -- part 2
+    print $ map fst . map (minimumBy (comparing snd)) $ input
