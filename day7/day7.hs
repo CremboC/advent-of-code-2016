05f7e207491ab55fb2284c8bce5151a16e19bf03
Paulius Imbrasas <cremboc@gmail.com>
2016-12-07 15:12:07 +0000

    Day 7: get rid of regexp

--- day7/day7.hs
+++ day7/day7.hs
@@ -1,22 +1,15 @@
-{-# LANGUAGE QuasiQuotes, FlexibleContexts #-}
-import Text.Regex.PCRE.Heavy
 import Data.List
-import Debug.Trace
+import Data.List.Split
 
-regexOut = [re|\[[a-z]+\]|]
-regexIn = [re|[a-z]+|]
-regexAbba = [re|([a-z])([a-z])\2\1|]
-regexAba = [re|([a-z])([a-z])\1|]
-
-parse :: String -> ([String], [String])
-parse ip = (matchOut, matchIn)
-    where
-        matchOut = split regexOut ip
-        matchIn = map (filter (`notElem` ['[', ']'])) . map fst $ scan regexOut ip
+parse :: String -> [[String]]
+parse ip = transpose . chunksOf 2 . concatMap (splitOn "]") . splitOn "[" $ ip
 
 hasAbba :: String -> Bool
 hasAbba "" = False
-hasAbba (a:b:c:d:xs) = if a == d && b == c && a /= b then True else hasAbba (b:c:d:xs)
+hasAbba (a:b:c:d:xs) = 
+    if a == d && b == c && a /= b 
+        then True 
+        else hasAbba (b:c:d:xs)
 hasAbba _ = False
 
 supportsAbba :: ([String], [String]) -> Bool
@@ -33,16 +26,19 @@ mkAba (a:b:c:xs) found =
         else mkAba (b:c:xs) found
 mkAba _ found = found
 
+mkBab :: String -> String
+mkBab (a:b:_) = b:a:b:[]
+
 supportsAba :: ([String], [String]) -> Bool
 supportsAba (outs, ins) = (/= 0) . length . filter abaHasBab $ abas
     where
         abas = concatMap (`mkAba` []) outs
         abaHasBab aba = (/= 0) . length . filter ((mkBab aba) `isInfixOf`) $ ins
-        mkBab (a:b:_) = b:a:b:[]
 
 main :: IO ()
 main = do
-    ips <- map parse . lines <$> readFile "input.txt"
+    ips <- map (\(a:b) -> (a, concat b)) . map parse . lines <$> readFile "input.txt"
+
     -- part 1
     print $ length . filter supportsAbba $ ips
 
