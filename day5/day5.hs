9e0138505acf524f83fcde321556d34fb0cdace0
Paulius Imbrasas <cremboc@gmail.com>
2016-12-07 15:47:09 +0000

    Day 5: small improvement

--- day5/day5.hs
+++ day5/day5.hs
@@ -9,19 +9,19 @@ input = "abbhdwsy"
 md5 x = hash x :: Digest MD5
 md5' x = show . md5 . pack $ x
 
-crack :: Int -> String -> String
-crack index password
-    | length password == 8 = password
-    | otherwise = if "00000" `isPrefixOf` hashed
-        then crack (succ index) (password ++ [hashed !! 5])
-        else crack (succ index) password
-    where hashed = md5' $ input ++ show index
+crack :: Int -> Int -> String
+crack index 0 = []
+crack index len = if "00000" `isPrefixOf` hashed 
+    then [hashed !! 5] ++ (crack (succ index) (pred len))
+    else crack (succ index) len
+    where 
+        hashed = md5' $ input ++ show index
 
 crack' :: Int -> Map.Map Int Char -> Map.Map Int Char
 crack' index password
     | length password == 8 = password
     | ("00000" `isPrefixOf` hashed) && (pos `Map.notMember` password) && (pos `elem` [0..7]) =
-        crack' (succ index) $ Map.insert pos char password
+        crack' (succ index) $! Map.insert pos char password
     | otherwise = crack' (succ index) password
     where
         (pos, char) = (digitToInt $ hashed !! 5, hashed !! 6)
@@ -29,6 +29,6 @@ crack' index password
 
 main = do
     -- part 1
-    print $ crack 0 ""
+    print $ crack 0 8
     -- part 2
-    print $ map snd $ sortBy (comparing fst) $ Map.toList $  crack' 0 Map.empty
+    print $ map snd $ sortBy (comparing fst) $ Map.toList $ crack' 0 Map.empty
