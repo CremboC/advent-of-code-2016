47723a70e0a1e4e4e4fa8e3626838998913730a9
Paulius Imbrasas <cremboc@gmail.com>
2016-12-08 11:27:20 +0000

    Day 8: longer, but more readable code

--- day8/day8.hs
+++ day8/day8.hs
@@ -1,24 +1,32 @@
 import Data.List
 import Data.List.Split
-import Text.Regex.Posix
 import Debug.Trace
 
 data Instruction = Rect Int Int | RotateCol Int Int | RotateRow Int Int deriving (Show, Eq)
 
 slice :: Int -> Int -> [a] -> [a]
-slice start end = take (end - start + 1) . drop start
+slice start end xs = take (end - start + 1) . drop start $ xs
+
+slice' :: Int -> Int -> [a] -> ([a], [a], [a])
+slice' start end xs = (pre, slice start end xs, post)
+    where 
+        (_, post) = splitAt (succ end) xs
+        (pre, _) = splitAt start xs
+
+splitAt' :: Int -> [a] -> ([a], [a], [a])
+splitAt' i xs = slice' i i xs
+
+parseRotate :: String -> (Int, Int)
+parseRotate str = (read n, read rot)
+    where 
+        [n', _, rot] = drop 2 . words $ str
+        n = last . splitOn "=" $ n' 
 
 parse :: String -> Instruction
 parse str 
-    | "rect" `isPrefixOf` str = 
-        let (_, _, _, [x, y]) = str =~ "rect ([0-9]+)x([0-9]+)" :: (String, String, String, [String]) in 
-            Rect (read x) (read y)
-    | "column" `isInfixOf` str = 
-        let (_, _, _, [x, rot]) = str =~ "column x=([0-9]+) by ([0-9]+)" :: (String, String, String, [String]) in 
-            RotateCol (read x) (read rot)
-    | "row" `isInfixOf` str = 
-        let (_, _, _, [y, rot]) = str =~ "row y=([0-9]+) by ([0-9]+)" :: (String, String, String, [String]) in 
-            RotateRow (read y) (read rot)
+    | "rect" `isPrefixOf` str = let [x, y] = splitOn "x" . last . words $ str in Rect (read x) (read y)
+    | "column" `isInfixOf` str = let (x, rot) = parseRotate str in RotateCol x rot
+    | "row" `isInfixOf` str = let (y, rot) = parseRotate str in RotateRow y rot
 
 mkRect :: Int -> Int -> [[Char]] -> [[Char]]
 mkRect x y state = state' ++ rest
@@ -28,20 +36,19 @@ mkRect x y state = state' ++ rest
         (change, rest) = splitAt y state
 
 rotateCol :: Int -> Int -> [[Char]] -> [[Char]]
-rotateCol x rot state = transpose state''
+rotateCol x rot state = transpose $ start ++ [col'] ++ end
     where 
-        state'' = (slice 0 (pred x) transposed) ++ [state'] ++ (slice (succ x) (length transposed) transposed)
-        state' = slice (len - rot) (len * 2 - rot - 1) (cycle col)
-        len = length col
-        col = transposed !! x
+        col' = slice (len - rot) (len * 2 - rot - 1) (cycle . head $ col)
+        len = length . head $ col
+        (start, col, end) = splitAt' x transposed
         transposed = transpose state
 
 rotateRow :: Int -> Int -> [[Char]] -> [[Char]]
-rotateRow y rot state = (slice 0 (pred y) state) ++ [state'] ++ (slice (succ y) (length state) state)
+rotateRow y rot state = start ++ [row'] ++ end
     where 
-        state' = slice (len - rot) (len * 2 - rot - 1) (cycle row)
-        len = length row
-        row = state !! y
+        (start, row, end) = splitAt' y state
+        row' = slice (len - rot) (len * 2 - rot - 1) (cycle . head $ row)
+        len = length . head $ row
 
 create :: [Instruction] -> [[[Char]] -> [[Char]]]
 create [] = [(\s -> s)] -- noop
@@ -61,5 +68,3 @@ main = do
     print $ length . filter (=='#') . concat . foldl (\state f -> f state) initialState $ f'
     -- part 2
     print $ foldl (\state f -> f state) initialState $ f'
-
-
