6e362e14fc06e7ce860093e77907d92d50c64877
Paulius Imbrasas <cremboc@gmail.com>
2016-12-10 12:58:43 +0000

    Day 10 Part 1: done

---
+++ day-10/day10.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import Data.List.Extra
+import qualified Data.Map.Strict as M
+import Data.Char
+import Debug.Trace
+
+type MinMax = [Int] -> Int
+type Bot = Int
+type Bin = Int
+type State = M.Map Bot [Int]
+data Giving = GBot Bot MinMax | GBin Bin MinMax
+
+isAlphaSpace :: Char -> Bool
+isAlphaSpace x = isAlpha x || isSpace x
+
+isGoes :: String -> Bool
+isGoes str = "value" `isPrefixOf` str
+
+parse str | isGoes str = let [chip, bot] = parsed in goes bot chip
+    where parsed = map (read :: String -> Int) . wordsBy isAlphaSpace $ str
+parse str = gives (read bot) (determine a aN minimum) (determine b bN maximum)
+    where 
+        determine s n f = if s == "bot" 
+            then GBot (read n) f
+            else GBin (read n) f
+        [_, bot, _, _, _, a, aN, _, _, _, b, bN] = words str
+
+goes :: Bot -> Int -> State -> (State, Bool)
+goes bot chip state = (giveChip bot chip state, True)
+
+-- bot give give state -> state'
+gives :: Bot -> Giving -> Giving -> State -> (State, Bool)
+gives bot g1 g2 state | canGive state = (state', True)
+    where
+        state' = f' g2 . f' g1 $ state
+        canGive s = checkBy ((== 2) . length) bot s
+        f' :: Giving -> State -> State
+        f' (GBot to minmax) s = transferChip bot to (minmax $ s M.! bot) s
+        f' (GBin to minmax) s = removeChip bot (minmax $ s M.! bot) s
+
+gives _ _ _ state = (state, False)
+
+transferChip :: Bot -> Bot -> Int -> State -> State
+transferChip from to chip state = removeChip from chip $ giveChip to chip state
+
+removeChip :: Bot -> Int -> State -> State
+removeChip from chip state = M.alter f from state
+    where
+        f Nothing = Nothing
+        f (Just chips) = Just (chips \\ [chip])
+
+giveChip :: Bot -> Int -> State -> State
+giveChip to chip state = M.alter f to state
+    where
+        f Nothing    = Just [chip]
+        f (Just old) = Just (old ++ [chip])
+
+checkBy :: (Ord k, Foldable f) => (f a -> Bool) -> k -> M.Map k (f a) -> Bool
+checkBy f k m = case M.lookup k m of
+    (Just is) -> f is
+    Nothing   -> False
+
+type Modifier = State -> (State, Bool)
+
+isTarget (bot, chips) = null $ [17, 61] \\ (sort chips)
+findTarget =  filter isTarget . filter ((== 2) . length . snd) . M.toList
+
+exec :: [Modifier] -> State -> State
+exec [] state = state
+exec (m:ms) state = if not . null $ foundTarget then state' else exec ms' state'
+    where
+        foundTarget = findTarget state
+        ms' = if modified then ms else ms ++ [m]
+        (state', modified) = m state
+
+main :: IO ()
+main = do
+    instructions <- fmap parse . lines <$> readFile "input.txt"
+    print $ findTarget $ exec instructions M.empty
