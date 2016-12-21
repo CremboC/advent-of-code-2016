97f09d3d7d0d0d05e36c0e5a7e20d5b5b35927d5
Paulius Imbrasas <cremboc@gmail.com>
2016-12-21 13:19:21 +0000

    Day 21: shorten code

--- day-21/day21parse.hs
+++ day-21/day21parse.hs
@@ -20,34 +20,13 @@ data Instruction = Reverse Int Int |
 int :: ParsecT Dec String a Int
 int = fromIntegral <$> L.integer
 
--- reverse positions 0 through 6
-reversei :: Parser Instruction
-reversei = Reverse <$> (string "reverse positions " *> int) <*> (string " through " *> int)
-
--- swap position 0 with position 1
-swapp :: Parser Instruction
-swapp = SwapP <$> (string "swap position " *> int) <*> (string " with position " *> int)
-
--- swap letter e with letter g
-swapl :: Parser Instruction
-swapl = SwapL <$> (string "swap letter " *> letterChar) <*> (string " with letter " *> letterChar)
-
-dir :: Parser Direction
-dir = f <$> choice [string "right", string "left"]
-    where f "left" = L; f _ = R
-          
--- rotate left 2 steps
--- rotate right 0 steps
-rotated :: Parser Instruction
-rotated = RotateD <$> (string "rotate " *> dir) <*> ((space *> int) <* string " step") <* many letterChar
-
--- rotate based on position of letter f
-rotatep :: Parser Instruction
-rotatep = RotateP <$> (string "rotate based on position of letter " *> asciiChar)
-
--- move position 3 to position 6
-move :: Parser Instruction
-move = Move <$> (string "move position " *> int) <*> (string " to position " *> int)
-
 parseAdvent :: String -> Maybe Instruction
 parseAdvent = parseMaybe (reversei <|> swapp <|> swapl <|> rotatep <|> rotated <|> move)
+    where
+        reversei = Reverse <$> (string "reverse positions " *> int) <*> (string " through " *> int)
+        swapp    = SwapP <$> (string "swap position " *> int) <*> (string " with position " *> int)
+        swapl    = SwapL <$> (string "swap letter " *> letterChar) <*> (string " with letter " *> letterChar)
+        dir      = f <$> choice [string "right", string "left"] where f "left" = L; f _ = R
+        rotated  = RotateD <$> (string "rotate " *> dir) <*> (space *> int <* string " step" <* optional (char 's'))
+        rotatep  = RotateP <$> (string "rotate based on position of letter " *> asciiChar)
+        move     = Move <$> (string "move position " *> int) <*> (string " to position " *> int)
